1111345e4026ca273b277c1e878038ee33846ad6
Reid Liu <Reid.liu@kuka.com>
2020-10-28 08:51:31 +0800

    add test

--- kotlin_basic_learn/src/main/kotlin/com/fantasybaby/kotlin/learn/channel/ChannelSender.kt
+++ kotlin_basic_learn/src/main/kotlin/com/fantasybaby/kotlin/learn/channel/ChannelSender.kt
@@ -3,17 +3,34 @@ package com.fantasybaby.kotlin.learn.channel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
-val channel = Channel<Int>()
-suspend fun receive(){
-     for (i in channel) {
-         println(i)
-     }
-}
-suspend fun main() {
 
-    GlobalScope.launch {
-        for (x in 1..5) channel.send(x * x)
+val channel = Channel<Int>()
+suspend fun receive() {
+//     for (i in channel) {
+//         println(i)
+//     }
+    while (true) {
+        val receive = channel.receive()
+        println("--$receive")
     }
-    receive()
+}
+
+suspend fun send() {
+    for (x in 1..5) channel.send(x * x)
+}
+
+suspend fun main() {
+    GlobalScope.launch {
+        receive()
+    }
+    GlobalScope.launch {
+        send()
+    }
+    channel.send(60)
     println("Done!")
+    Thread.sleep(10000)
+    GlobalScope.launch {
+        send()
+    }
+    Thread.sleep(10000)
 }
